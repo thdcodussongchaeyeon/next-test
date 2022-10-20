bac745a731e83cf3a5f4605b41c970bcb8c6bf68
thdcodussongchaeyeon <thd0628@naver.com>
2022-10-20 11:58:03 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title> Home - Next Tutorial</title>'
+        <title> Home - Next Tutorial</title>
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome Home</h1>
